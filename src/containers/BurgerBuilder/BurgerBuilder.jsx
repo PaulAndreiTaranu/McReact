d3f863357ededc6d100f819ea7e950d92c7a503e
Paul Andrei <paulandreitaranu@gmail.com>
2020-08-08 17:50:17 +0100

    Deploy

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -11,7 +11,7 @@ import withErrorHandling from "../../hoc/withErrorHandling/withErrorHandling";
 import * as actions from "../../store/actions/index";
 import axios from "../../axios-orders";
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
    state = {
       purchasing: false,
    };
